bb52dad7bc63214e4fa2c36a20edc0594416be10
Aayush Kasera <aayushksr@gmail.com>
2023-08-13 22:18:12 +0530

    cors error removed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,15 @@ const notesrouter = require("./atrangi notes");
 const router = require("./atrangi todo list");
 const path = require("path");
 const buildpath = "../../frontend/build";
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "https://aayush-kasera-portfolio.onrender.com/",
+  })
+);
 
 const port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, buildpath)));
-app.use(cors());
 app.use(notesrouter);
 app.use(router);
 
